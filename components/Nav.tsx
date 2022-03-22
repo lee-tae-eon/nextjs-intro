a6e9128aedeaa2eb779d568347da2097fa4dc40d
lee-tae-eon <mktears0609@gmail.com>
2022-03-22 21:53:54 +0900

    styled jsx

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,32 +1,28 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-import styles from "./Nav.module.css";
-
 export default function Nav() {
   const router = useRouter();
 
   return (
     <nav>
       <Link href="/">
-        <a
-          className={`${router.pathname === "/" ? styles.active : ""} ${
-            styles.link
-          }`}
-        >
-          Home
-        </a>
+        <a className={router.pathname === "/" ? "active" : ""}>Home</a>
       </Link>
       <Link href="/about">
-        <a
-          className={[
-            styles.link,
-            router.pathname === "/about" ? styles.active : "",
-          ].join(" ")}
-        >
-          About
-        </a>
+        <a className={router.pathname === "/about" ? "active" : ""}>About</a>
       </Link>
+      <style jsx>{`
+        nav {
+          background-color: tomato;
+        }
+        a {
+          text-decoration: none;
+        }
+        .active {
+          color: yellow;
+        }
+      `}</style>
     </nav>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <Nav />
-      <h1>hello</h1>
+      <h1 className="active">hello</h1>
     </div>
   );
 }
