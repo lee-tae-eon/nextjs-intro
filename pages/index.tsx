84f7bf72430e1bcbcd4f0bb220ad6502bdf3a6e2
lee-tae-eon <mktears0609@gmail.com>
2022-03-26 23:32:20 +0900

    fetch movie

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,32 @@
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
+const API_KEY = "6c2a18c3a603a48a447e48c59ffd5b1a";
+
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+
+      setMovies(results);
+    })();
+  }, []);
+
   return (
     <div>
       <Seo title={"Home"} />
-
-      <h1 className="active">hello</h1>
+      {!movies && <h4>loading...</h4>}
+      {movies?.map((movie: any) => (
+        <div key={movie?.id}>
+          <h4>{movie?.title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
